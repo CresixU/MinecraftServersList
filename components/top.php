ea531222c074d9bc9e9fb173df6b340482638a9d
Krystian Wawruch <CresixU@gmail.com>
2023-02-26 13:20:25 +0100

    Logout button

--- components/top.php
+++ components/top.php
@@ -4,15 +4,11 @@
                 <div class="col-6">
                     <div class="menu">
                         <ul>
-                            <li id="nav-konto" style="position: relative;">
-                                <a href="account.php">Konto</a>
-                                <ol style="display: none">
-                                    <li>Wyloguj</li>
-                                </ol>
-                            </li>
+                            <li id="nav-konto"><a href="account.php">Konto</a></li>
                             <li id="nav-serwery" ><a href="index.php">Serwery</a></li>
                             <li id="nav-partnerzy" ><a href="partners.php">Partnerzy</a></li>
                             <li id="nav-statystyki" ><a href="">Statystyki</a></li>
+                            <li id="nav-logout" style="display:none" onclick="Logout()"><a>Wyloguj</a></li>
                         </ul>
                     </div>
                 </div>
@@ -35,3 +31,22 @@
             </div>
         </div>
     </header>
+    <?php $api = require("config.php"); ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script>
+        var api_url = "<?php echo $api ?>";
+        $.ajax({
+            url: api_url+'/api/v1/auth/logged/',
+            success: function() {
+                $('#nav-logout').css('display','list-item');
+            }
+        })
+
+        function Logout() {
+            $.ajax({
+                url: api_url+'/api/v1/auth/logout/',
+                type: 'DELETE',
+                success: console.log('logout'),
+            })
+        }
+    </script>
