f66aa29eba68d84c5835cb5084d4a46908a29a08
Krystian Wawruch <CresixU@gmail.com>
2023-02-26 01:06:26 +0100

    Create partners.php

---
+++ partners.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>MinecraftList</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="autocomplete/tokenize2.css">
+        <style>
+            body {
+                color: #dfd7cc;
+            }
+
+        </style>
+    </head>
+    <body>
+        <?php $api = require("config.php"); ?>
+        <div id="fb-root"></div>
+        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v11.0&appId=915876171902531&autoLogAppEvents=1" nonce="k7fGxMia"></script>
+        <?php require_once("components/top.php"); ?>
+        <main>
+            <div class="container">
+                <div class="row">
+                    <div class="col col-12">
+                        <div class="panel">
+                            <div class="panel-header">
+                                <div class="panel-header-title">
+                                    NASI PARTNERZY
+                                </div>
+                            </div>
+                            <div class="panel-content p-3 pt-5">
+                    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <div class="container">
+
+            </div>
+        </footer>
+        <div class="copyright">
+            Lista serwerów Minecraft, spis serwerów Minecraft, prywatne serwery Minecraft, serwery Minecraft - &copy; Copyright 2013-2021
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        <script>
+            var api_url = "<?php echo $api ?>";
+            var data;
+            var userData;
+
+            $('#nav-partnerzy').addClass('active');
+            
+        </script>
+        
+    </body>
+</html>
