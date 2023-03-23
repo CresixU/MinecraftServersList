30d9350e9c9655df6e7ca3af20f4539aced73e4b
Krystian Wawruch <CresixU@gmail.com>
2023-03-23 10:22:27 +0100

    Added Scrollable modal

--- admin-servers.php
+++ admin-servers.php
@@ -113,7 +113,7 @@
 
             <!-- MODAL EDIT -->
                                     <div class="modal fade" id="modal_edit" tabindex="-1" role="dialog" aria-labelledby="modal_edit" aria-hidden="true">
-                                        <div class="modal-dialog" role="document">
+                                        <div class="modal-dialog modal-dialog-scrollable" role="document">
                                             <div class="modal-content">
                                                 <div class="modal-header">
                                                     <h5 class="modal-title">Edytowanie serwera</h5>
