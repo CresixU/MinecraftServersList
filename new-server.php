053a6f23c8cb79ee533445fc8826d99fb30aa107
Krystian Wawruch <CresixU@gmail.com>
2023-03-25 18:19:29 +0100

    Moved script to other file

--- new-server.php
+++ new-server.php
@@ -6,6 +6,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
         <link rel="stylesheet" href="autocomplete/tokenize2.css">
+        <link rel="stylesheet" href="css/ckeditor.css">
         <style>
             body {
                 color: #dfd7cc;
@@ -167,17 +168,7 @@
         <script src="//code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
         <script src="https://www.google.com/recaptcha/api.js?render=6Ldj08kkAAAAAOAR7XBwQsbBnsFMfQFGAwE5qusl"></script>
         <script src="https://cdn.ckeditor.com/ckeditor5/36.0.1/classic/ckeditor.js"></script>
-        <script>
-            var editor;
-            ClassicEditor
-                .create( document.querySelector( '#editor' ) )
-                .then( newEditor => {
-                    editor = newEditor;
-                } )
-                .catch( error => {
-                    console.error( error );
-                } );
-        </script>
+        <script type="module" src="js/ckeditor.js"></script>
         <script>
             var api_url = "<?php echo $api ?>";
             var data;
