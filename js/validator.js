4ec9e29f35dd8fa64377e8d67fda407c4062a618
Krystian Wawruch <CresixU@gmail.com>
2023-03-30 16:28:27 +0200

    Create validator.js

---
+++ js/validator.js
@@ -0,0 +1,11 @@
+//Return false on fail
+function ValidateInput(id) {
+    if($(id).val().length == 0 || $(id).val() == null || $(id).val() == ' ') {
+        $(id).css("border-bottom","2px solid red");
+        return false;
+    }
+    else {
+        $(id).css("border-bottom", "2px solid var(--main-color)");
+        return true;
+    }
+}
