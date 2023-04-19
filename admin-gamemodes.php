ba129073b7206d5e91ac1c21dbeb0c623b0caa9c
Krystian Wawruch <CresixU@gmail.com>
2023-04-19 21:56:59 +0200

    Changed copyright info and created component for it

--- admin-gamemodes.php
+++ admin-gamemodes.php
@@ -156,9 +156,7 @@
 
             </div>
         </footer>
-        <div class="copyright">
-            Lista serwerów Minecraft, spis serwerów Minecraft, prywatne serwery Minecraft, serwery Minecraft - &copy; Copyright 2013-2021
-        </div>
+        <?php include_once("components/copyright.php"); ?>
 
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
---
+++ components/copyright.php
@@ -0,0 +1,6 @@
+<div class="copyright">
+    Lista serwerów Minecraft, spis serwerów Minecraft, prywatne serwery Minecraft, serwery Minecraft - &copy; Copyright 2013-<span id="copyright-date"></span>
+</div>
+<script>
+    document.getElementById('copyright-date').innerHTML = new Date().getFullYear();
+</script>
