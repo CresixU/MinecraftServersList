0817d344b92494cf113390660e9101ae2de90a71
Krystian Wawruch <CresixU@gmail.com>
2023-06-24 13:35:21 +0200

    Add site description

--- server.php
+++ server.php
@@ -3,6 +3,7 @@
 <head>
     <title>MinecraftList</title>
     <link rel="stylesheet" href="css/style.css">
+    <meta name="description" content="Serwery Minecraft - Znajdź i promuj swoje serwery Minecraft. Minecraft-List.pl to platforma, na której możesz odkrywać różnorodne serwery Minecraft, znaleźć społeczność graczy i promować swój własny serwer. Przeglądaj setki serwerów, oceniaj je i wybieraj najlepsze miejsce do gry. Dodawaj swój serwer na naszej stronie i dotrzyj do szerokiego grona graczy. Zapraszamy do świata Minecraft na Minecraft-List.pl!">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/35076142fc.js" crossorigin="anonymous"></script>
     
